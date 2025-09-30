af6b6102452e4ff12782bb12440842e7afeb755b
Hayk Movsisyan <haykmovsisyan01@gmail.com>
2025-09-30 12:18:05 +0400

    / by 0 fix.(2)

--- lib/presentation/providers/bloc/quiz/quiz_bloc.dart
+++ lib/presentation/providers/bloc/quiz/quiz_bloc.dart
@@ -47,11 +47,6 @@ class QuizBloc extends Bloc<QuizEvent, QuizState> {
   }
 
   finishQuiz(QuizFinishEvent event, Emitter<QuizState> emit) {
-    if (correctAnswers == 0) {
-      emit(
-        QuizResultState(Result(correctAnswers: 0, wrongAnswers: wrongAnswers)),
-      );
-    }
     emit(
       QuizResultState(
         Result(correctAnswers: correctAnswers, wrongAnswers: wrongAnswers),
